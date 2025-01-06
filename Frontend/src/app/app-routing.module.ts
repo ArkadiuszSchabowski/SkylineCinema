91acfd813a13f29c2e41413e4876ff5301889896
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-06 17:05:05 +0100

    chore: add routing for components

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/public/home/home.component';
+import { ReservationComponent } from './components/public/reservation/reservation.component';
+import { NowPlayingComponent } from './components/public/now-playing/now-playing.component';
+import { TrailersComponent } from './components/public/trailers/trailers.component';
+import { ComingSoonComponent } from './components/public/coming-soon/coming-soon.component';
+import { PriceListComponent } from './components/public/price-list/price-list.component';
+import { ContactComponent } from './components/public/contact/contact.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent}
+  {path: "", component: HomeComponent},
+  {path: "reservation", component: ReservationComponent},
+  {path: "now-playing", component: NowPlayingComponent},
+  {path: "trailers", component: TrailersComponent},
+  {path: "coming-soon", component: ComingSoonComponent},
+  {path: "price-list", component: PriceListComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "**", component: HomeComponent}
 ];
 
 @NgModule({
