5c8d1d68fab03f545d6d4c37c79ebf306e66bed5
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-06 21:15:36 +0100

    feat: add local setCity method linked to service method

--- Frontend/src/app/components/public/home/home.component.ts
+++ Frontend/src/app/components/public/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CityService } from 'src/app/_services/city.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-
+export class HomeComponent{
+  constructor(private cityService: CityService){
+  }
+  
+  setCity(city: string){
+    this.cityService.setCity(city);
+  }
 }
