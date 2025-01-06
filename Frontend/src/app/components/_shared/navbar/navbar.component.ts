c659d48a671cc801fe42f72c371e12825a835c37
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-06 21:17:12 +0100

    feat: add local getCity method linked with city$ in service

--- Frontend/src/app/components/_shared/navbar/navbar.component.ts
+++ Frontend/src/app/components/_shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CityService } from 'src/app/_services/city.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  city: string | null = null;
+  constructor(private cityService: CityService){
+    this.getCity();
+  }
+  getCity(){
+    this.cityService.city$.subscribe(city => {
+      this.city = city;
+    })
+  }
 }
