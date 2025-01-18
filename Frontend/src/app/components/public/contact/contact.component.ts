11af4f5d55ed2801a9a0708262e3b722f1cbd0ad
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-18 14:16:56 +0100

    feat: add trySendEmail and isValidEmail functions

--- Frontend/src/app/components/public/contact/contact.component.ts
+++ Frontend/src/app/components/public/contact/contact.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ContactDto } from 'src/app/_models/contact-dto';
+import { ContactErrorDto } from 'src/app/_models/contact-error-dto';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  model: ContactDto = new ContactDto();
+  errors: ContactErrorDto = new ContactErrorDto();
 
+  constructor(private toastr: ToastrService) {}
+
+  trySendEmail(contactForm: NgForm) {
+    this.errors = new ContactErrorDto();
+
+    if (!this.model.email) {
+      this.errors.email = 'E-mail jest wymagany!';
+    } else if (!this.isValidEmail(this.model.email)) {
+      this.errors.email = 'Niepoprawny format e-maila!';
+    }
+
+    if (!this.model.name || this.model.name.length < 3) {
+      this.errors.name = 'Imię musi zawierać co najmniej 3 znaki';
+    }
+
+    if (!this.model.message || this.model.message.length < 25) {
+      this.errors.message = 'Wiadomość musi zawierać co najmniej 25 znaków';
+    }
+
+    if (this.errors.email || this.errors.name || this.errors.message) {
+      return;
+    }
+
+    contactForm.resetForm();
+
+    //TO DO - implement API message validation implemantation and handle response
+
+    this.toastr.success('Wiadomość została wysłana!');
+  }
+
+  private isValidEmail(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
 }
