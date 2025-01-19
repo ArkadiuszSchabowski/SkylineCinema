298eae0902097e1103f183d729f4a8b8693dc4b9
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-19 13:24:30 +0100

    bugfix: fix Angular test issues

--- Frontend/src/app/components/_shared/navbar/navbar.component.spec.ts
+++ Frontend/src/app/components/_shared/navbar/navbar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularMaterialModule } from 'src/app/_modules/angular-material/angular-material.module';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,7 +9,11 @@ describe('NavbarComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NavbarComponent]
+      declarations: [NavbarComponent],
+      imports: [
+        AngularMaterialModule,
+        RouterTestingModule
+      ]
     });
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
--- Frontend/src/app/components/auth/register/register.component.spec.ts
+++ Frontend/src/app/components/auth/register/register.component.spec.ts
@@ -1,17 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { ToastrService } from 'ngx-toastr';
+import { AngularMaterialModule } from 'src/app/_modules/angular-material/angular-material.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let toastrService: ToastrService;
+
+  class MockToastrService {
+    success(message: string) {
+      console.log(message);
+    }
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RegisterComponent]
+      declarations: [RegisterComponent],
+      imports: [AngularMaterialModule, FormsModule, BrowserAnimationsModule],
+      providers: [{ provide: ToastrService, useClass: MockToastrService }],
     });
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService);
     fixture.detectChanges();
   });
 
