e38b1bd54bfd17a36b5b71256a930b8fd15ad34e
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-01-21 12:57:11 +0100

    feat: add initial values for reservation template

--- Frontend/src/app/components/public/reservation/reservation.component.ts
+++ Frontend/src/app/components/public/reservation/reservation.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
-  styleUrls: ['./reservation.component.scss']
+  styleUrls: ['./reservation.component.scss'],
 })
 export class ReservationComponent {
+
+  tickets: any[] = [];
+  selectedTicket: any = "";
+
+  firstFormGroup = this.formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this.formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  thirdFormGroup = this.formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+
+  constructor(private formBuilder: FormBuilder) {}
 
 }
